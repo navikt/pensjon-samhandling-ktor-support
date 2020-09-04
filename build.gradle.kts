8f234c54c2739fcd2d44dcb4c2fd4baba6899d18
a150238 <eivind.askestad@nav.no>
2020-09-04 12:43:02 +0200

    Publishing in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,17 +5,35 @@ group = "no.nav.pensjonsamhandling"
 plugins {
     kotlin("jvm") version "1.4.0"
     kotlin("plugin.serialization") version "1.4.0"
+    `maven-publish`
 }
 
 repositories {
     jcenter()
     mavenCentral()
     maven("https://packages.confluent.io/maven/")
-    maven("https://jitpack.io")
 }
 
 dependencies {
 
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/navikt/${rootProject.name}")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
 
 java {

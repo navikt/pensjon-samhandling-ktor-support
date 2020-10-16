7a55ca2a2ededad4ddd3d43ca17edc12ab364e09
a150238 <eivind.askestad@nav.no>
2020-10-16 11:05:08 +0200

    env functionality public. OMG

--- src/main/kotlin/no/nav/pensjon/samhandling/env/EnvironmentSupport.kt
+++ src/main/kotlin/no/nav/pensjon/samhandling/env/EnvironmentSupport.kt
@@ -1,14 +1,20 @@
 package no.nav.pensjon.samhandling.env
 
-internal fun Map<String, String>.getVal(key: String) = this[key] ?: throw MissingEnvironmentVariable("""$key, is not found in environment""")
+fun Map<String, String>.getVal(key: String) =
+    this[key] ?: throw MissingEnvironmentVariable("""$key, is not found in environment""")
 
-internal fun Map<String, String>.getVal(key: String, defaultValue: String) = this.getOrDefault(key, defaultValue)
+fun Map<String, String>.getVal(key: String, defaultValue: String) = this.getOrDefault(key, defaultValue)
 
-internal fun Map<String, String>.verifyEnvironmentVariables(keys: List<String>) {
+fun Map<String, String>.verifyEnvironmentVariables(keys: List<String>) {
     val missingKeys = keys.filter { this[it] == null }
-    if (missingKeys.isNotEmpty()) throw MissingEnvironmentVariables(missingKeys.joinToString(", ", "Missing env keys: "))
+    if (missingKeys.isNotEmpty()) throw MissingEnvironmentVariables(
+        missingKeys.joinToString(
+            ", ",
+            "Missing env keys: "
+        )
+    )
 }
 
-internal class MissingEnvironmentVariable(message: String) : RuntimeException(message)
+class MissingEnvironmentVariable(message: String) : RuntimeException(message)
 
-internal class MissingEnvironmentVariables(message: String) : RuntimeException(message)
+class MissingEnvironmentVariables(message: String) : RuntimeException(message)

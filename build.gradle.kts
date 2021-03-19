d9b1d1d692c312b6d9aecd87a112e024c90a4ba2
a150238 <eivind.askestad@nav.no>
2021-03-19 07:37:38 +0100

    Removed maskinporten integration. Use pensjon-opptjening-gcp-maskinporten-client instead!!

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val ktorVersion = "1.3.2"
 val micrometerRegistryPrometheusVersion = "1.1.5"
 
-val maskinportenClientVersion = "0.3.4"
-val joseJwtVersion = "9.0.1"
-
 val wiremockVersion = "2.27.2"
 val junitJupiterVersion = "5.6.0"
 
@@ -37,9 +34,6 @@ dependencies {
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
     implementation("io.ktor:ktor-metrics-micrometer:$ktorVersion")
     implementation("io.micrometer:micrometer-registry-prometheus:$micrometerRegistryPrometheusVersion")
-
-    implementation("no.nav.pensjonsamhandling:maskinporten-client:$maskinportenClientVersion")
-    implementation("com.nimbusds:nimbus-jose-jwt:$joseJwtVersion")
 
     testImplementation("com.github.tomakehurst:wiremock:$wiremockVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
